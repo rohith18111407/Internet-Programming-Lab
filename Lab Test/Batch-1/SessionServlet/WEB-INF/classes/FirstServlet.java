a7dcc67585d5b2e5d36bbdc7634f702c9592ccfa
Rohith M <108167442+rohith18111407@users.noreply.github.com>
2024-05-13 19:47:09 +0530

    Add files via upload

---
+++ Lab Test/Batch-1/SessionServlet/WEB-INF/classes/FirstServlet.java
@@ -0,0 +1,32 @@
+import java.io.*;  
+// import jakarta.servlet.*;  
+// import jakarta.servlet.http.*; 
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+// import javax.servlet.ServletException;
+  
+  
+public class FirstServlet extends HttpServlet {  
+  
+public void doGet(HttpServletRequest request, HttpServletResponse response){  
+        try{  
+  
+        response.setContentType("text/html");  
+        PrintWriter out = response.getWriter();  
+          
+        // String n=request.getParameter("userName");  
+        
+          
+        HttpSession session=request.getSession();  
+        // session.setAttribute("uname",n);  
+  
+        out.print("<a href='servlet2'>Count</a>");  
+                  
+        out.close();  
+  
+        }
+        catch(Exception e){System.out.println(e);}  
+    }  
+  
+} 
---
+++ Lab Test/Batch-1/SessionServlet/WEB-INF/classes/SecondServlet.java
@@ -0,0 +1,41 @@
+// import java.io.*;
+// import jakarta.servlet.*;
+// import jakarta.servlet.http.*;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class SecondServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+
+            // Get session
+            HttpSession session = request.getSession(true);
+
+            // Get count from session attribute
+            Integer count = (Integer) session.getAttribute("count");
+
+            // If count is null, initialize it to 0
+            if (count == null) {
+                count = 0;
+            }
+
+            // Increment count
+            count++;
+
+            // Update session attribute
+            session.setAttribute("count", count);
+
+            out.print("Number of times the visit link has been clicked: " + count);
+
+            out.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
